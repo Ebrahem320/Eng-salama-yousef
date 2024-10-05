5452025a90e94deb86b599f3cbfae263f3201402
Ebrahem salama shahat <132373364+Ebrahem320@users.noreply.github.com>
2024-10-05 22:39:06 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('nav ul li a');
-    links.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const section = document.querySelector(this.getAttribute('href'));
-            window.scrollTo({
-                top: section.offsetTop,
-                behavior: 'smooth'
-            });
-        });
-    });
-});
+
